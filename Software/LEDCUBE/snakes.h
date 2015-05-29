dec60901d05e13a6ad8512c36f0b6b8ff69d1141
chuby <chubylive@gmail.com>
2015-05-29 17:20:41 -0400

    coming along

--- Software/LEDCUBE/snakes.h
+++ Software/LEDCUBE/snakes.h
@@ -1,6 +1,19 @@
 #ifndef _SNAKES_h_
 #define _SNAKES_h_
 
-#inlcude "ledcube.h"
+#include "ledcube.h"
+#define NORTH 0
+#define SOUTH 1
+#define EAST 2
+#define WEST 3
 
+
+typedef struct snake_node {
+	uint8_t is_head;
+	uint8_t x;	
+	uint8_t y;
+	uint8_t z;
+	uint8_t direction;
+	struct snake_node *next_ptr;
+}s_node;
 
