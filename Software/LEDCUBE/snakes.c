03f2685b568e1c99b9cfb19a96bf0ea8b39d4fc4
chuby <chubylive@gmail.com>
2015-06-02 16:34:27 -0400

    Update snakes.c

--- Software/LEDCUBE/snakes.c
+++ Software/LEDCUBE/snakes.c
@@ -1,5 +1,34 @@
 #include "snakes.h"
 
-void animate(struct *snakes){
+void animate(){
+	for (int idx = 0; idx < MAX_SNAKES; idx++)
+	{
+		/* code */
+		update_snake(s_array[idx]);
+	}
+}
 
+void update_snake(s_node head){
+	//bounds check
+	s_node * curr_node = head;
+	while(curr_node->next_ptr != NULL){
+		if(curr_node->x > SIZE){
+			return;
+		}
+		if(curr_node->y > SIZE){
+			return;
+		}
+		if(curr_node->z > SIZE){
+			return;
+		}
+
+		//clear old image
+
+		//update image
+
+		//update next image
+
+		curr_node = curr_node->next_ptr;
+	}
+	
 }
