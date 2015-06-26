4aae4ea91b935c4ed47612ee3ae4d89f7e238ca0
chuby <chubylive@gmail.com>
2015-06-26 15:01:20 -0400

    Update snakes.h

--- Software/LEDCUBE/snakes.h
+++ Software/LEDCUBE/snakes.h
@@ -6,14 +6,27 @@
 #define SOUTH 1
 #define EAST 2
 #define WEST 3
-
+#define MAX_SNAKES 10
+#define MAX_S_LENGTH 256
+typedef struct cord{
+	uint8_t x;
+	uint8_t y;
+	uint8_t z;
+}cord_t; 
 
 typedef struct snake_node {
 	uint8_t is_head;
-	uint8_t x;	
-	uint8_t y;
-	uint8_t z;
+	cord_t old_cord;
+	cord_t new_cord;
 	uint8_t direction;
-	struct snake_node *next_ptr;
-}s_node;
+	uint8_t speed;
+	COLOUR clr;
+	uint8_t length;
+}s_node_t;
 
+
+typedef struct snake {
+	s_node_t s_array[MAX_S_LENGTH];
+}snake_t;
+
+snake_t snake_array[MAX_SNAKES];
