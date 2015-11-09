1c20ec694a3267fc3475393dd5fa408d9fcb6637
chuby okafor <chubylive@gmail.com>
2015-11-08 19:16:31 -0500

    f

--- CMakeFiles/boot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/boot"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/boot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- CMakeFiles/gdb.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/gdb"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/gdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
